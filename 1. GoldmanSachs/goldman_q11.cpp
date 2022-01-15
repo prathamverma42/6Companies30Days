e34337f0b76a7bfd5960b97d403b58d99ded5fa9
Pratham Verma <prathamverma12345@gmail.com>
2022-01-15 12:17:45 +0530

    codes added

--- 1. GoldmanSachs/goldman_q11.cpp
+++ 1. GoldmanSachs/goldman_q11.cpp
@@ -1,21 +1,20 @@
 class Solution{
-public:	
-	// #define ull unsigned long long
-	/* Function to get the nth ugly number*/
-	ull getNthUglyNo(int n) {
-	    // code here
-	    vector<long long int> v(n+1);
-	    long long int c2=0,c3=0,c5=0;
-	    v[0]=1;
-	    for(int i=1;i<=n;i++){
-	        v[i]=min({2*v[c2],3*v[c3],5*v[c5]});
-	        if(v[i]==2*v[c2])
-	        c2++;
-	        if(v[i]==3*v[c3])
-	        c3++;
-	        if(v[i]==5*v[c5])
-	        c5++;
-	    }
-	    return v[n-1];
-	}
+public:
+    int *findTwoElement(int *arr, int n) {
+        // code here
+        int *ans =  new int[2];
+        for(int i=0;i<n;i++){
+            if(arr[abs(arr[i])-1]<0){
+                ans[0]=abs(arr[i]);            
+            }else{
+                arr[abs(arr[i])-1]=-arr[abs(arr[i])-1];
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]>0){
+                ans[1]=i+1;
+            }
+        }
+        return ans;
+    }
 };
